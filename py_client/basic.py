c9e6aa9707c301a8f1f61dab0fedb4ad0b26c4f4
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-01-23 13:54:04 -0600

    api views

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,12 @@
 import requests
 
 # endpoint = 'https://httpbin.org/anything'
-endpoint = 'http://localhost:8000/'
+endpoint = 'http://localhost:8000/api/'
 
-get_response = requests.get(endpoint, json={'query': 'hello world'})
-print(get_response.text) # print raw text of html document
-print(get_response.status_code)
+get_response = requests.get(endpoint, json={'product_id': 123}, )
+# print(get_response.headers)
+# print(get_response.text) # print raw text of html document
+# print(get_response.status_code)
 
-# print(get_response.json())
+print(get_response.json())
 # print(get_response.status_code)
