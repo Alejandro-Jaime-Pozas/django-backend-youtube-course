a6fa4835e1613c7eeb58d90969d1c5b29b44a2fd
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-01-22 18:53:22 -0600

    s

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,7 @@
 import requests
 
 # endpoint = 'https://httpbin.org/anything'
-endpoint = 'http://127.0.0.1:8000/'
+endpoint = 'http://localhost:8000/'
 
 get_response = requests.get(endpoint, json={'query': 'hello world'})
 print(get_response.text) # print raw text of html document
