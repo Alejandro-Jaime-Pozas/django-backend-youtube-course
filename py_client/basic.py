8f1b785c25d89f60c06d01169a729458257a5a39
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-01-22 18:49:31 -0600

    first

---
+++ py_client/basic.py
@@ -0,0 +1,11 @@
+import requests
+
+# endpoint = 'https://httpbin.org/anything'
+endpoint = 'http://127.0.0.1:8000/'
+
+get_response = requests.get(endpoint, json={'query': 'hello world'})
+print(get_response.text) # print raw text of html document
+print(get_response.status_code)
+
+# print(get_response.json())
+# print(get_response.status_code)
