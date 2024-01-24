bdc7393826528c40aab72493a3f834ae2cacb707
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-01-24 13:35:41 -0600

    serializers

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,8 @@ import requests
 # endpoint = 'https://httpbin.org/anything'
 endpoint = 'http://localhost:8000/api/'
 
-get_response = requests.get(endpoint, json={'product_id': 123}, )
+# get_response = requests.get(endpoint, json={'product_id': 123}, )
+get_response = requests.post(endpoint, json={'title': 'gryffindor', 'content': 'Hello World', 'price': 13.98})
 # print(get_response.headers)
 # print(get_response.text) # print raw text of html document
 # print(get_response.status_code)
